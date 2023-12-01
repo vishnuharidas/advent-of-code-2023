ffa1e0044349dfed878068c953a37aa53eb2a4a5
Vishnu Haridas <hvishnu999@gmail.com>
2023-12-01 18:07:57 +0400

    Solve second puzzle

--- src/main/kotlin/day01/solution.kt
+++ src/main/kotlin/day01/solution.kt
@@ -5,7 +5,7 @@ import util.getFileLines
 fun main() {
 
     // Problem 1
-    val total = getFileLines("day01/input.txt").sumOf { line ->
+    val result1 = getFileLines("day01/input.txt").sumOf { line ->
         line.firstOrNull { it.isDigit() }
             ?.digitToInt()
             ?.times(10)
@@ -16,6 +16,26 @@ fun main() {
             ) ?: 0
     }
 
-    println("Problem 1 output: $total")
+    println("Problem 1 output: $result1")
 
+    val numbers = listOf(
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        "1", "2", "3", "4", "5", "6", "7", "8", "9"
+    )
+
+    val result2 = getFileLines("day01/input.txt").sumOf { line ->
+
+        val first = line.findAnyOf(numbers)?.second?.anyToDigit() ?: 0
+        val last = line.findLastAnyOf(numbers)?.second?.anyToDigit() ?: 0
+
+        first * 10 + last
+
+    }
+
+    println("Problem 2 output: $result2")
 }
+
+private fun String.anyToDigit(): Int = listOf(
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+).indexOf(this) % 10
